ca65f8b6cf4d582d88c1b93801387aec9008d68a
petervanderhook <petervanderhook@gmail.com>
2022-10-03 02:01:51 -0700

    Added 17 jobs, 19 gallery photos. Fixed lightbox and removed redundant scripts.

--- work.js
+++ work.js
@@ -4,6 +4,8 @@ const base_work_container = document.getElementById("work-container")
 const parent = document.getElementsByClassName('viewbox')[0];
 let json_data = []
 
+const jobtype = document.currentScript.getAttribute("jobtype")
+
 fetch(workRequest)
     .then(response => {
         if (!response.ok) {
@@ -18,29 +20,9 @@ fetch(workRequest)
         front_pic.src = response[0].images[0]
         container.querySelector('.viewbox-img').appendChild(front_pic)
         front_pic.addEventListener('load', () => {
-            const width = front_pic.width
-            const height = front_pic.height
-            let view_height = document.defaultView.getComputedStyle(container).height;
-            let view_width = document.defaultView.getComputedStyle(container).width;
-            view_height = +view_height.replace('px', '')
-            view_width = +view_width.replace('px', '')
-            width_diff = width / view_width
-            height_diff = height / view_height
-            
-            if (width > height) {
-                if (width > view_width) {
-                    front_pic.height = height * (view_width / width);
-                    front_pic.width = view_width;
-                }
-            } else {
-                if (height > view_height) {
-                    front_pic.width = width * (view_height / height);
-                    front_pic.height = view_height;
-                }
-            }
-
+            front_pic.style.objectFit = 'contain'
             json_data = response
-            load_page("deck")
+            load_page(jobtype)
 
         })
     })
@@ -54,6 +36,7 @@ for (let i=0;i<close_buttons.length;i++) {
     close_buttons[i].addEventListener("click", () => {
         parent.style.display = 'none'
     })
+    console.log("Running")
     
 }
 
@@ -77,21 +60,35 @@ function create_job(entry) {
     new_job.style.backgroundPosition = 'center'
     
     new_job.addEventListener('click', () => {
+        // Remove event listeners (Jank)
+        const lef_arrow = document.querySelector('.content-left-arrow')
+        const rig_arrow = document.querySelector('.content-right-arrow')
+        lef_arrow.replaceWith(lef_arrow.cloneNode(true))
+        rig_arrow.replaceWith(rig_arrow.cloneNode(true))
+        const left_arrow = document.querySelector('.content-left-arrow')
+        const right_arrow = document.querySelector('.content-right-arrow')
+
+        // Set Title and Text
+        let content_title = document.querySelector('.content-title')
+        let content_text = document.querySelector('.content-text')
+        content_title.innerHTML = entry.title
+        content_text.innerHTML = entry.description
+
+
         parent.style.display = 'flex'
         let current_image_index = 0
         const img_container = parent.querySelectorAll('img')
         console.log("entry:", entry.images)
+
         if (entry.images.length > 0) {
             img_container[0].src = `${entry.images[current_image_index]}`
             /* content-left-arrow, content-right-arrow */
-            const left_arrow = document.querySelector('.content-left-arrow')
-            const right_arrow = document.querySelector('.content-right-arrow')
             console.log(left_arrow, right_arrow)
 
             left_arrow.addEventListener('click', () => {
-                console.log(`Previous image index: ${current_image_index}`)
                 current_image_index -= 1
-                console.log(current_image_index)
+                //console.log(`Current image index: ${current_image_index}`)
+                console.log(entry.images.length)
                 if (current_image_index < 0) {
                     // If you pass over to -1 index value, add the length of the array to index value before setting source
                     current_image_index += entry.images.length
@@ -100,9 +97,8 @@ function create_job(entry) {
 
             })
             right_arrow.addEventListener('click', () => {
-                console.log(`Previous image index: ${current_image_index}`)
                 current_image_index += 1
-                console.log(current_image_index)
+                //console.log(`Current image index: ${current_image_index}`)
                 if (current_image_index >= entry.images.length) {
                     // If you pass over to -1 index value, add the length of the array to index value before setting source
                     current_image_index -= entry.images.length
@@ -114,6 +110,12 @@ function create_job(entry) {
         }
         else {
             img_container[0].src = './img/unavailable.png'
+            right_arrow.addEventListener('click', () => {
+                console.log('No Pictures.')
+            })
+            left_arrow.addEventListener('click', () => {
+                console.log('No Pictures.')
+            })
         }
     })
     let new_hover_element = document.createElement('div')
